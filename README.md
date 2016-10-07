3c0ac12ea3396be6ba0e668fdbbf6b2bab365cc2
hujianxia <hujianxia@caiqr.com>
2016-10-07 10:34:40 +0800

    commit readme mainjs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #git原理解析和准备资料
+##ADD COMMIT PUSH
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 window.onload = function(){
     console.log('this is mainjs first');
+    if(true){
+      console.log('the autor is zero');
+    }
 }
